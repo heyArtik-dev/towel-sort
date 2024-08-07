fb7f87c58f9bee8307d3e863dcba0086f0007abf
heyArtik <heyartik@rambler.ru>
2024-08-07 19:07:52 +0300

    init: start towelsort adn done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+
+    let result = [];
+
+    if (!matrix) return result;
+    if (matrix.length == 0) return result;
+
+    for (let i = 0; i < matrix.length; i++) {
+        if (i % 2 !== 0) matrix[i].reverse();
+    }
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            result.push(matrix[i][j]);
+        }
+    }
+
+    return result;
 }
+
